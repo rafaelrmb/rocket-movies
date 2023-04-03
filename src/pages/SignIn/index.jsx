8e5449be8d995c6b7a0d19fbe214d5666991ca5d
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-03 16:09:11 -0300

    add auth context

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,18 +2,39 @@ import { Container, Form, Background } from "./styles";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { RiMailLine, RiLockLine } from "react-icons/ri";
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function SignIn() {
+	const { signIn } = useAuth();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleSignIn(e) {
+		e.preventDefault();
+		signIn({ email, password });
+	}
+
 	return (
 		<Container>
 			<Form>
 				<h1>RocketMovies</h1>
 				<p>Aplicação para acompanhar tudo que assistir!</p>
 				<p>Faça seu login na plataforma</p>
-				<Input placeholder="Digite seu email" type="email" icon={RiMailLine} />
-				<Input placeholder="Digite sua senha" type="password" icon={RiLockLine} />
-				<Button title="Entrar" />
-				<a href="/">Criar conta</a>
+				<Input
+					placeholder="Digite seu email"
+					type="email"
+					icon={RiMailLine}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Input
+					placeholder="Digite sua senha"
+					type="password"
+					icon={RiLockLine}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<Button title="Entrar" onClick={handleSignIn} />
+				<a href="/register">Criar conta</a>
 			</Form>
 			<Background />
 		</Container>
