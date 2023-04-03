13ef2f790fed7c4fa54532f647ea23556852a2cd
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-03 16:07:52 -0300

    add useAuth hook

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from "react";
+import { api } from "../services/api";
+
+const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+	const [data, setData] = useState({});
+
+	async function signIn({ email, password }) {
+		try {
+			const res = await api.post("sessions", {
+				email,
+				password,
+			});
+
+			const { user, token } = res.data;
+
+			api.defaults.headers.Authorization = `Bearer ${token}`;
+			setData({ user, token });
+		} catch (err) {
+			alert(err.response.data.error);
+		}
+	}
+
+	return (
+		<AuthContext.Provider value={{ signIn, user: data.user }}>{children}</AuthContext.Provider>
+	);
+}
+
+function useAuth() {
+	const context = useContext(AuthContext);
+
+	return context;
+}
+
+export { AuthProvider, useAuth };
