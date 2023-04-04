b4117e4c07ece72c20fdc3fe6740f9bd0dd070e8
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-04 16:14:33 -0300

    add delete movie functionality

--- src/pages/MovieDetails/index.jsx
+++ src/pages/MovieDetails/index.jsx
@@ -1,16 +1,44 @@
 import { Container, Info } from './styles';
 import { Header } from '../../components/Header';
 import { Tag } from '../../components/Tag';
-import { RiArrowLeftLine, RiStarFill, RiStarLine, RiTimeLine } from 'react-icons/ri';
-import { Link, useParams } from 'react-router-dom';
+import {
+	RiArrowLeftLine,
+	RiStarFill,
+	RiStarLine,
+	RiTimeLine,
+	RiDeleteBin2Fill,
+} from 'react-icons/ri';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
 import { useState, useEffect } from 'react';
+import { Button } from '../../components/Button';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 export function MovieDetails() {
 	const { id } = useParams();
+	const navigate = useNavigate();
+
 	const [data, setData] = useState(null);
+
 	const user = JSON.parse(localStorage.getItem('@rocketmovies:user')) || {};
 	const { name, avatar } = user;
+
+	async function handleDeleteMovie() {
+		const confirm = window.confirm('Deseja realmente excluir este filme?');
+
+		if (confirm) {
+			try {
+				await api.delete(`/movies/${id}`).then(() => {
+					setData(null);
+					navigate('/');
+				});
+			} catch (err) {
+				console.log(err.response.data.error);
+			}
+		} else {
+			return alert('Operação cancelada');
+		}
+	}
 
 	useEffect(() => {
 		async function getMovie() {
@@ -49,12 +77,19 @@ export function MovieDetails() {
 			{data && (
 				<main>
 					<Info>
-						<Link to='/'>
-							<p>
-								<RiArrowLeftLine />
-								Voltar
-							</p>
-						</Link>
+						<div className='actions'>
+							<Link to='/'>
+								<p>
+									<RiArrowLeftLine />
+									Voltar
+								</p>
+							</Link>
+							<Button
+								title='Excluir filme'
+								icon={RiDeleteBin2Fill}
+								onClick={handleDeleteMovie}
+							/>
+						</div>
 
 						<div id='movie-info'>
 							<h1>{title}</h1>
@@ -64,7 +99,7 @@ export function MovieDetails() {
 						<div id='created-by'>
 							<p>
 								<img
-									src={`${api.defaults.baseURL}files/${avatar}`}
+									src={avatar ? `${api.defaults.baseURL}files/${avatar}` : avatarPlaceholder}
 									alt='foto de perfil do usuario'
 								/>
 								Por {name}
