319c41a6cf2328f2193aeabde95f670834c7c241
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-04 10:00:32 -0300

    add tags add and remove functionality

--- src/pages/NewMovie/index.jsx
+++ src/pages/NewMovie/index.jsx
@@ -1,18 +1,33 @@
-import { RiArrowLeftLine } from "react-icons/ri";
-import { Container, Form, Section } from "./styles";
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
-import { TextArea } from "../../components/TextArea";
-import { Button } from "../../components/Button";
-import { MovieTag } from "../../components/MovieTag";
-import { Link } from "react-router-dom";
+import { RiArrowLeftLine } from 'react-icons/ri';
+import { Container, Form, Section } from './styles';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { TextArea } from '../../components/TextArea';
+import { Button } from '../../components/Button';
+import { MovieTag } from '../../components/MovieTag';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export function NewMovie() {
+	const [tags, setTags] = useState([]);
+	const [newTag, setNewTag] = useState('');
+
+	function handleAddTag(e) {
+		e.preventDefault();
+		setTags((prevState) => [...prevState, newTag]);
+		setNewTag('');
+	}
+
+	function handleRemoveTag(e, deletedTag) {
+		e.preventDefault();
+		setTags((prevState) => prevState.filter((tag) => tag !== deletedTag));
+	}
+
 	return (
 		<Container>
 			<Header />
 			<main>
-				<Link to="/">
+				<Link to='/'>
 					<a>
 						<RiArrowLeftLine />
 						Voltar
@@ -20,19 +35,42 @@ export function NewMovie() {
 				</Link>
 				<Form>
 					<h2>Novo filme</h2>
-					<Input placeholder="Título" type="text" />
-					<Input placeholder="Sua nota (de 0 a 5)" type="number" min="0" max="5" />
-					<TextArea placeholder="Observações" />
+					<Input
+						placeholder='Título'
+						type='text'
+					/>
+					<Input
+						placeholder='Sua nota (de 0 a 5)'
+						type='number'
+						min='0'
+						max='5'
+					/>
+					<TextArea placeholder='Observações' />
 					<Section>
 						<span>Marcadores</span>
-						<div id="tags">
-							<MovieTag value="Action" />
-							<MovieTag value="Comedy" />
-							<MovieTag value="Novo marcador" isNew={true} />
+						<div id='tags'>
+							{tags.map((tag, index) => (
+								<MovieTag
+									key={String(index)}
+									value={tag}
+									isNew={false}
+									onClick={(e) => handleRemoveTag(e, tag)}
+								/>
+							))}
+							<MovieTag
+								placeholder='Adicione um marcador'
+								value={newTag}
+								isNew={true}
+								onChange={(e) => setNewTag(e.target.value)}
+								onClick={handleAddTag}
+							/>
 						</div>
 					</Section>
-					<Button title="Excluir filme" id="delete-btn" />
-					<Button title="Salvar alterações" />
+					<Button
+						title='Excluir filme'
+						id='delete-btn'
+					/>
+					<Button title='Salvar alterações' />
 				</Form>
 			</main>
 		</Container>
