783de45e42132dd921fd77efbf7175ad6508b839
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-04 17:26:32 -0300

    add email and password validation

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { Container, Form, Background } from "../SignIn/styles";
-import { Button } from "../../components/Button";
-import { Input } from "../../components/Input";
-import { RiMailLine, RiLockLine, RiUserLine } from "react-icons/ri";
-import { api } from "../../services/api";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { Container, Form, Background } from '../SignIn/styles';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { RiMailLine, RiLockLine, RiUserLine } from 'react-icons/ri';
+import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 export function SignUp() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
 	const navigate = useNavigate();
 
@@ -17,18 +17,31 @@ export function SignUp() {
 		e.preventDefault();
 
 		if (!name || !email || !password) {
-			return alert("Preencha todos os campos");
+			return alert('Preencha todos os campos');
+		}
+
+		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+		if (!emailRegex.test(email)) {
+			return alert('Invalid email address');
+		}
+
+		const passwordRegex =
+			/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=[\]{}|\\;:'",.<>/?-]).{8,}$/;
+		if (!passwordRegex.test(password)) {
+			return alert(
+				'Password must be at least 8 characters long, contain at least one digit, one lowercase letter, one uppercase letter, and one special character'
+			);
 		}
 
 		api
-			.post("/users", {
+			.post('/users', {
 				name,
 				email,
 				password,
 			})
 			.then(() => {
 				alert(`Usuário ${name} cadastrado com sucesso!`);
-				navigate("/");
+				navigate('/');
 			})
 			.catch((err) => {
 				alert(err.response.data.error);
@@ -42,25 +55,28 @@ export function SignUp() {
 				<p>Aplicação para acompanhar tudo que assistir!</p>
 				<p>Criar sua conta</p>
 				<Input
-					placeholder="Nome"
-					type="text"
+					placeholder='Nome'
+					type='text'
 					icon={RiUserLine}
 					onChange={(e) => setName(e.target.value)}
 				/>
 				<Input
-					placeholder="Email"
-					type="email"
+					placeholder='Email'
+					type='email'
 					icon={RiMailLine}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<Input
-					placeholder="Senha"
-					type="password"
+					placeholder='Senha'
+					type='password'
 					icon={RiLockLine}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-				<Button title="Cadastrar" onClick={handleSignUp} />
-				<a href="/">Já possui uma conta? Faça login</a>
+				<Button
+					title='Cadastrar'
+					onClick={handleSignUp}
+				/>
+				<a href='/'>Já possui uma conta? Faça login</a>
 			</Form>
 			<Background />
 		</Container>
