216e6a5eb7a4aa42f45f5ee750ddb701803473b9
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-04 11:50:02 -0300

    formatting

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,6 +1,6 @@
-import { Container } from "./styles";
-import { Tag } from "../../components/Tag";
-import { RiStarFill, RiStarLine } from "react-icons/ri";
+import { Container } from './styles';
+import { Tag } from '../../components/Tag';
+import { RiStarFill, RiStarLine } from 'react-icons/ri';
 
 export function MovieCard({ data, ...rest }) {
 	const rating = Math.round(data.rating); // round the rating to the nearest integer
@@ -16,11 +16,14 @@ export function MovieCard({ data, ...rest }) {
 	return (
 		<Container {...rest}>
 			<h2>{data.title}</h2>
-			<div className="rating">{stars}</div>
-			<p id="description">{data.description}</p>
-			<div className="tags">
+			<div className='rating'>{stars}</div>
+			<p id='description'>{data.description}</p>
+			<div className='tags'>
 				{data.tags.map((tag) => (
-					<Tag key={tag.id} title={tag.name} />
+					<Tag
+						key={tag.id}
+						title={tag.name}
+					/>
 				))}
 			</div>
 		</Container>
