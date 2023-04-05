13dd48d12ade0a221afe29927b17bd5060ebc21b
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-05 14:45:10 -0300

    add navigation to register page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,14 +1,16 @@
-import { Container, Form, Background } from "./styles";
-import { Button } from "../../components/Button";
-import { Input } from "../../components/Input";
-import { RiMailLine, RiLockLine } from "react-icons/ri";
-import { useAuth } from "../../hooks/auth";
-import { useState } from "react";
+import { Container, Form, Background } from './styles';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { RiMailLine, RiLockLine } from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function SignIn() {
 	const { signIn } = useAuth();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
 
 	function handleSignIn(e) {
 		e.preventDefault();
@@ -22,19 +24,22 @@ export function SignIn() {
 				<p>Aplicação para acompanhar tudo que assistir!</p>
 				<p>Faça seu login na plataforma</p>
 				<Input
-					placeholder="Digite seu email"
-					type="email"
+					placeholder='Digite seu email'
+					type='email'
 					icon={RiMailLine}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<Input
-					placeholder="Digite sua senha"
-					type="password"
+					placeholder='Digite sua senha'
+					type='password'
 					icon={RiLockLine}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-				<Button title="Entrar" onClick={handleSignIn} />
-				<a href="/register">Criar conta</a>
+				<Button
+					title='Entrar'
+					onClick={handleSignIn}
+				/>
+				<a onClick={navigate('/register')}>Criar conta</a>
 			</Form>
 			<Background />
 		</Container>
